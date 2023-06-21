045ce52f0df33934199478d8845d8fa0cc977946
russelelele <adam.le.chance@gmail.com>
2023-06-21 21:33:18 +0300

    Create update-1.sql

---
+++ update-1.sql
@@ -0,0 +1,12 @@
+update fine set sum_fine=sum_fine*2 
+where name = any (select name from (select name, number_plate, violation 
+ from fine group by name, number_plate, violation 
+ having count(*)=2) as x) 
+and number_plate= any (select number_plate 
+from (select name, number_plate, violation 
+from fine
+ group by name, number_plate, violation 
+ having count(*)=2) as xx) 
+ and date_payment is null; 
+ 
+ select * from fine
