bb38b775872e6f924acdd546b1467e23cdbd6236
russelelele <adam.le.chance@gmail.com>
2023-06-21 21:40:22 +0300

    Create CREATE TABLE book (.sql

---
+++ CREATE TABLE book (.sql
@@ -0,0 +1,12 @@
+CREATE TABLE book (
+    book_id INT PRIMARY KEY AUTO_INCREMENT, 
+    title VARCHAR(50), 
+    author_id INT NOT NULL, 
+    genre_id INT,
+    price DECIMAL(8,2), 
+    amount INT, 
+    FOREIGN KEY (genre_id) references genre (genre_id)on delete set null, 
+    FOREIGN KEY (author_id)  REFERENCES author (author_id)  on delete cascade 
+);
+
+select * from book
